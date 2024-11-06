00cf032dd12ac0e8fc6eef4425b8c35b9276849f
Paul Watts <paul.watts@me.com>
2024-11-06 21:43:40 +1100

    Add exercise 05 amd 06

---
+++ 05-Printing to the Screen/ex05.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This is printed.\n";
+	std::cerr << "This writes to the error output.\n";
+
+	// notice the \t at the start
+	std::cout << "\tThis is tabbed in.\n";
+
+	return 0;
+}
